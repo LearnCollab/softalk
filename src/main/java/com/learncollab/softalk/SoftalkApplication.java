0e6e462a7f5d8cc956b13b413a75bcc4ae988b9f
jhy <hha0303@naver.com>
2023-08-30 12:53:57 +0900

    init

---
+++ src/main/java/com/learncollab/softalk/SoftalkApplication.java
@@ -0,0 +1,13 @@
+package com.learncollab.softalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learncollab/softalk/SoftalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learncollab.softalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
