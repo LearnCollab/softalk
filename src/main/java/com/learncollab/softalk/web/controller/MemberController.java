edc9cbb54e2c06df44d35175e8c78ef137ab842d
eujjong18 <eujjong18@naver.com>
2023-10-20 00:04:34 +0900

    [feat #21]: 이메일 인증번호 전송 컨트롤러 구현

--- src/main/java/com/learncollab/softalk/web/controller/MemberController.java
+++ src/main/java/com/learncollab/softalk/web/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.learncollab.softalk.web.controller;
 
+import com.learncollab.softalk.domain.dto.member.EmailVerificationReqDto;
 import com.learncollab.softalk.domain.dto.member.JoinDto;
 import com.learncollab.softalk.domain.dto.member.JwtToken;
 import com.learncollab.softalk.exception.member.MemberException;
@@ -100,6 +101,15 @@ public class MemberController {
         if (memberService.findMemberByName(name).isPresent()) {
             throw new MemberException(NAME_ALREADY_EXIST, NAME_ALREADY_EXIST.getCode(), NAME_ALREADY_EXIST.getErrorMessage());
         }
+    }
+
+
+    /*이메일 인증번호 전송 API*/
+    @PostMapping("/email/code-request")
+    public ResponseEntity<String> requestVerificationCode(
+            @RequestBody EmailVerificationReqDto.sendCodeRequest request) {
+        // TODO memberService.sendCodeToEmail(request);
+        return ResponseEntity.ok("인증번호 발송 성공");
     }
 
 }
