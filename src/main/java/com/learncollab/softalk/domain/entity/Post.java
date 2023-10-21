dac09c0319952e6469a8aeefcbe75e6ac3d4ae03
eujjong18 <eujjong18@naver.com>
2023-10-21 21:53:35 +0900

    [feat #22]: 커뮤니티 내 게시글(Post) 엔티티 설계

---
+++ src/main/java/com/learncollab/softalk/domain/entity/Post.java
@@ -0,0 +1,31 @@
+package com.learncollab.softalk.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "posts")
+@NoArgsConstructor
+@Getter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "community_id", nullable = false)
+    private Community community;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+}
